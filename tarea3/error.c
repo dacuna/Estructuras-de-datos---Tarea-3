09a9ebe38d66081c181805eb99374ab495203e14
Diego Acuña <dacuna@inf.utfsm.cl>
2009-05-30 01:17:34 -0400

    Actualizacion: Proyecto anjuta borrado

--- tarea3/error.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "error.h"
-
-void error(char *msg){
-	printf("%s\n",msg);
-	exit(0);
-}	
--- tarea3/error.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ERROR_H_
-#define _ERROR_H_
-
-#define EMPTY_STACK "Pila vacia"
-#define EMPTY_QUEUE "Cola vacia"
-
-void error(char *msg);
-
-#endif
